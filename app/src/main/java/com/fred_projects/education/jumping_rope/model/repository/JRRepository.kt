b3b12ab700fc743726102139ef5fd700fb2e65cb
FredNekrasov <frednekrasov49@gmail.com>
2024-02-29 20:44:04 +0300

    refactoring the JRRepository

--- app/src/main/java/com/fred_projects/education/jumping_rope/model/repository/JRRepository.kt
+++ app/src/main/java/com/fred_projects/education/jumping_rope/model/repository/JRRepository.kt
@@ -5,16 +5,8 @@ import com.fred_projects.education.jumping_rope.model.entity.JRReps
 import kotlinx.coroutines.flow.Flow
 
 class JRRepository(private val dao: IJRDao) : IJRRepository {
-    override suspend fun insertOrUpdateRecord(jR: JRReps) {
-        dao.insertRecord(jR)
-    }
-    override suspend fun deleteRecord(jR: JRReps) {
-        dao.deleteRecord(jR)
-    }
-    override fun getData(): Flow<List<JRReps>> {
-        return dao.getData()
-    }
-    override suspend fun getRecord(id: Int): JRReps? {
-        return dao.getRecord(id)
-    }
+    override suspend fun insertOrUpdateRecord(jR: JRReps) = dao.insertRecord(jR)
+    override suspend fun deleteRecord(jR: JRReps) = dao.deleteRecord(jR)
+    override fun getData(): Flow<List<JRReps>> = dao.getData()
+    override suspend fun getRecord(id: Int): JRReps? = dao.getRecord(id)
 }
