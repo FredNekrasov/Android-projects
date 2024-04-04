b0a1c8b467d2ba3e52cbef672d0716879740a813
FredNekrasov <frednekrasov49@gmail.com>
2024-04-04 19:09:04 +0300

    changing the endpoint of the astronomical API

--- app/src/main/java/com/fred_projects/education/rest_api/astronomy/model/service/IAstronomyService.kt
+++ app/src/main/java/com/fred_projects/education/rest_api/astronomy/model/service/IAstronomyService.kt
@@ -3,8 +3,9 @@ package com.fred_projects.education.rest_api.astronomy.model.service
 import com.fred_projects.education.rest_api.astronomy.model.service.dto.StarInfoDTO
 import retrofit2.http.*
 
+//https://api.astrocats.space/catalog?ra=21:23:32.16&dec=-53:01:36.08&radius=2
 interface IAstronomyService {
-    @GET("/sne/catalog/")
+    @GET("/catalog/")
     suspend fun getMoreInfo(
         @Query("ra") ra: String,
         @Query("dec") dec: String,
